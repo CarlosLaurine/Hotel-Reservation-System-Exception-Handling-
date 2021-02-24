12b74d5c8eae323ad67fe54db572685a02f0553e
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-02-24 20:57:12 -0300

    Reservation model entity class Finished

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,66 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	private static SimpleDateFormat sdf1 = new SimpleDateFormat ("dd/MM/yyyy");
+
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	public long duration(Date checkin, Date checkout) {
+		long difference = checkin.getTime() - checkout.getTime();
+		return TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);		
+	}
+	
+	public void updatedDates(Date checkin, Date checkout) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", Check-In: "
+			+ sdf1.format(checkin)
+			+ ", Check-Out: "
+			+ sdf1.format(checkout)
+			+ ", Duration: "
+			+ duration(checkin, checkout)
+			+ "Nights";
+		
+	}
+	
+}
